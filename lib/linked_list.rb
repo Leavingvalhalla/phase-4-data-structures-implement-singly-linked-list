ccd9a55fc5dcfcf5141924752208f8953392fb65
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-07 16:06:09 -0700

    commit tree 97c6d3ca7e54624d0ca5e92b384e6e9617453509

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,68 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+        if @head == nil
+            @head = node
+        else
+            x = @head
+            @head = node
+            @head.next_node = x
+        end
+    end
+        
+    def append(node)
+        if @head == nil
+            @head = node
+        else
+            last = @head
+            while last.next_node
+                last = last.next_node
+            end
+            last.next_node = node
+        end
+    end
+
+    def delete_head
+        @head && @head = @head.next_node
+    end
+
+    def delete_tail
+        if @head && @head.next_node
+            last = @head
+            while last.next_node.next_node
+                last = last.next_node
+            end
+            last.next_node = nil
+        elsif @head
+            @head = nil
+        end
+    end
+
+    def add_after(index, node)
+        target = @head
+       (index).times do
+        if target.next_node
+            target = target.next_node
+        end
+        end
+        old_node = target.next_node
+        target.next_node = node
+        node.next_node = old_node
+    end
+
+    def search(value)
+        node = @head
+        while node do
+            return node if node.data == value 
+            node = node.next_node
+        end
+        count
+    end
 
 end
